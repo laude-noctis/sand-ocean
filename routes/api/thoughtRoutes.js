b9c00777c5c0fe09d4fbbc2c43370fe19f2e8271
unknown <lizzybumpers11@gmail.com>
2024-02-02 12:52:56 -0600

    added routes one thought and create thought

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,20 +2,49 @@ const { ObjectId } = require('mongoose').Types;
 const { Thought } = require('../../models');
 const router = require('express').Router();
 
+// get all thoughts
 router.get('/', async (req, res) => {
     console.log('Getting all thoughts')
     try {
-        const thought = await Thought.find({})
+        const allThoughts = await Thought.find({})
             .populate({ path: "reactions", select: "-__v" });
 
-        res.json(thought)
+        res.json(allThoughts)
     } catch (err) {
         console.error(err)
         res.status(500).json(err)
     }
 })
 
+// get one thought
+router.get('/:thoughtId', async (req, res) => {
+    console.log('Getting one thought')
+    try {
+        const oneThought = await Thought.findOne({ _id: req.params.thoughtId})
+        .populate({ path: "reactions", select: "-__v" });
 
+        if (!oneThought) {
+            return res.status(404).json({ message: "No thoughts here"})
+        }
+
+        res.json(oneThought)
+    } catch (err) {
+        console.error(err)
+        res.status(500).json(err)
+    }
+})
+
+// creating a thought
+router.post('/', async (req, res) => {
+    console.log('Creating a thought')
+    try {
+        const newThought = await Thought.create(req.body)
+        res.json(newThought)
+    } catch (err) {
+        console.error(err)
+        res.status(500).json(err)
+    }
+})
 
 router.post('/:thoughtId/reactions', async (req, res) => {
     console.log('Creating a reaction')

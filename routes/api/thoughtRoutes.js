93bd753b2f450bc409e79eac26d3c9155974eeb6
unknown <lizzybumpers11@gmail.com>
2024-02-02 13:19:12 -0600

    trying to figure out how to get the user/username assciated with the thought

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,5 +1,5 @@
 const { ObjectId } = require('mongoose').Types;
-const { Thought } = require('../../models');
+const { Thought, User } = require('../../models');
 const router = require('express').Router();
 
 // get all thoughts
@@ -20,11 +20,11 @@ router.get('/', async (req, res) => {
 router.get('/:thoughtId', async (req, res) => {
     console.log('Getting one thought')
     try {
-        const oneThought = await Thought.findOne({ _id: req.params.thoughtId})
-        .populate({ path: "reactions", select: "-__v" });
+        const oneThought = await Thought.findOne({ _id: req.params.thoughtId })
+            .populate({ path: "reactions", select: "-__v" });
 
         if (!oneThought) {
-            return res.status(404).json({ message: "No thoughts here"})
+            return res.status(404).json({ message: "No thoughts here" })
         }
 
         res.json(oneThought)
@@ -39,22 +39,32 @@ router.post('/', async (req, res) => {
     console.log('Creating a thought')
     try {
         const newThought = await Thought.create(req.body)
+        const user = await User.findOneAndUpdate(
+            { _id: req.body.userId },
+            { $addToSet: { thoughts: newThought._id } },
+            { new: true },
+        );
+
+        if (!user) {
+            res.status(404).json({ message: 'Thought created, but no user thought it' })
+        }
+
         res.json(newThought)
     } catch (err) {
         console.error(err)
         res.status(500).json(err)
     }
-})
-
-router.post('/:thoughtId/reactions', async (req, res) => {
-    console.log('Creating a reaction')
-    try {
-        const createReaction = await Thought.create(req.body)
-        res.json(createReaction)
-    } catch (err) {
-        console.error(err)
-        res.status(500).json(err)
-    }
 });
+
+// router.post('/:thoughtId/reactions', async (req, res) => {
+//     console.log('Creating a reaction')
+//     try {
+//         const createReaction = await Thought.create(req.body)
+//         res.json(createReaction)
+//     } catch (err) {
+//         console.error(err)
+//         res.status(500).json(err)
+//     }
+// });
 
 module.exports = router
